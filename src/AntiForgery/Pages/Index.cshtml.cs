f043441641d945ff2c7bf0f8c4c2705d44e23114
TheAlexDev23 <thealexdev23@gmail.com>
2022-03-12 16:36:43 +0100

    Initial commit

---
+++ src/AntiForgery/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AntiForgery.Pages;
+
+public class IndexModel : PageModel {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger) {
+        _logger = logger;
+    }
+
+    public void OnGet() {
+
+    }
+
+
+    [BindProperty]
+    public LoginModel LogInBindingModel {get; set;}
+
+
+    [ValidateAntiForgeryToken]
+    public void OnPost() {
+        _logger.LogInformation(LogInBindingModel.UserName);
+    }
+
+    public class LoginModel {
+        [StringLength(maximumLength: 150, ErrorMessage = "Name cannot be larger than 150 characters")]
+        [Required(ErrorMessage = "This field is required")]
+        public string UserName {get; set;}
+    }
+}
